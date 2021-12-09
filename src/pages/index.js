d57ea9b870221f424b38cbc0358a34bdbbb6958b
pauliescanlon <paul.scanlon@gatsbyjs.com>
2021-12-08 22:20:25 -0500

    feat: remove about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,6 @@ const Page = () => {
   return (
     <main>
       <h1>Index Page</h1>
-      <p>
-        This is an example template for helping you to learn how to build a site
-        powered by Gatsby
-      </p>
-      <Link to="/about">About</Link>
     </main>
   );
 };
