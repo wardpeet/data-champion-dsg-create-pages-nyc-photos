51799d3e40199e3deb3db93cd4232fa6e4a98574
pauliescanlon <paul.scanlon@gatsbyjs.com>
2021-12-15 19:58:20 -0500

    feat: refactor defer

--- gatsby-node.js
+++ gatsby-node.js
@@ -73,7 +73,7 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
         prev: index === 0 ? null : previous,
         next: index === edges.length - 1 ? null : next
       },
-      defer: likes < 100 ? true : false
+      defer: likes < 100
     });
   });
 };
